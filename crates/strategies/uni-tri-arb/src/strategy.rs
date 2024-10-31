024acc5946e515dbd9dd68466566f2998a634665
dvisacker <davidvanisacker@gmail.com>
2024-10-31 18:22:50 +0900

    fix - update addressbook loading call

--- crates/strategies/uni-tri-arb/src/strategy.rs
+++ crates/strategies/uni-tri-arb/src/strategy.rs
@@ -52,7 +52,7 @@ pub struct UniTriArb<P: Provider + 'static, S: Signer> {
 
 impl<P: Provider + 'static, S: Signer> UniTriArb<P, S> {
     pub fn new(chain: Chain, client: Arc<P>, signer: S, db_url: String) -> Self {
-        let addressbook = Addressbook::load().unwrap();
+        let addressbook = Addressbook::load(None).unwrap();
         let weth = addressbook.get_weth(&chain.named().unwrap()).unwrap();
         Self {
             chain,
