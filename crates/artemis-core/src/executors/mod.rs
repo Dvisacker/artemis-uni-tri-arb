2f8649d3622a62461f7f34fbe58deb47df23ae87
dvisacker <davidvanisacker@gmail.com>
2024-11-05 23:09:24 +0900

    chore - remove crosschain executor

--- crates/artemis-core/src/executors/mod.rs
+++ crates/artemis-core/src/executors/mod.rs
@@ -2,7 +2,6 @@
 //! executing them in different domains. For example, an executor might take a
 //! `SubmitTx` action and submit it to the mempool.
 
-pub mod crosschain_executor;
 pub mod mempool_executor;
 /// This executor submits transactions to the public mempool.
 pub mod sequence_executor;
