e55348ce70a3a75d74c503e90f03a7874f74207c
dvisacker <davidvanisacker@gmail.com>
2024-11-12 21:46:04 +0900

    feat - add batch executor deployment script

---
+++ contracts/script/DeployBatchExecutor.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {BatchExecutor} from "../src/BatchExecutor.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract DeployAaveLooper is Script {
+    address owner;
+
+    function run() external {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.getActiveNetworkConfig();
+
+        owner = networkConfig.deployerAddress;
+        require(owner != address(0), "Owner address not set");
+
+        vm.startBroadcast();
+        BatchExecutor executor = new BatchExecutor(owner);
+        console2.log("BatchExecutor deployed at:", address(executor));
+        vm.stopBroadcast();
+    }
+}
